6c7287b828f4244cce4ab60047b3be60bd7550e8
wangshuzhe <wangshuzhe@tjhq.com>
2025-04-05 13:32:40 +0800

    初始化项目文件服务器

---
+++ fuds/src/test/java/com/ws/fjj/fuds/FudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.fjj.fuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
