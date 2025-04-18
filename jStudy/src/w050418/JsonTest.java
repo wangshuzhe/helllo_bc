66855b4db541e7fd0c555a99b5f0ba6bfbce2197
wangshuzhe <wangshuzhe@tjhq.com>
2025-04-18 15:33:10 +0800

    string 的两种判断非空方法

---
+++ jStudy/src/w050418/JsonTest.java
@@ -0,0 +1,9 @@
+package w050418;
+
+public class JsonTest {
+    public static void main(String[] args) {
+        String s = "{\"abc\": \"\"}";
+        //StringUtils.IsNotEmpty() 检测字符串长度大于 0
+        //StringUitls.IsNotBlank() 检测字符串长度大于 0 且至少包含一个非空白符
+    }
+}
