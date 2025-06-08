71ca67cc4f207fe0518b8eadfb787f6f9425f08e
wangshuzhe <wangshuzhe@tjhq.com>
2025-06-08 12:40:55 +0800

    Junit测试

---
+++ LinkUp/src/main/java/junitT/chapter5/JettySample.java
@@ -0,0 +1,14 @@
+package junitT.chapter5;
+
+
+import javax.naming.Context;
+
+public class JettySample {
+    public static void main(String[] args) {
+/*
+        Server server = new Server(8080);
+        Context root = new Context(server, "/");
+
+*/
+    }
+}
