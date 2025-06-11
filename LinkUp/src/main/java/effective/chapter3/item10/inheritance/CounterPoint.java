bddd396015f7bcb3a06e1c18a9e5968a231cde53
wangshuzhe <wangshuzhe@tjhq.com>
2025-06-11 10:35:26 +0800

    子父类的 equals 和 hashcode 方法实践

--- LinkUp/src/main/java/effective/chapter3/item10/inheritance/CounterPoint.java
+++ LinkUp/src/main/java/effective/chapter3/item10/inheritance/CounterPoint.java
@@ -4,6 +4,11 @@ import effective.chapter3.item10.Point;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+// Trivial subclass of Point - doesn't add a value component (Page 43)
+
+/**
+ * Point 的平凡子类，不添加值组件
+ */
 public class CounterPoint extends Point {
     private static final AtomicInteger counter =
             new AtomicInteger();
