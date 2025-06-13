9433f067fa76f35e4da1f79f41afb42f2f050e0a
wangshuzhe <wangshuzhe@tjhq.com>
2025-06-13 16:18:47 +0800

    feat 不可变复数类

---
+++ LinkUp/src/main/java/effective/chapter4/item17/Complex.java
@@ -0,0 +1,66 @@
+package effective.chapter4.item17;
+//不可变复数类
+public class Complex {
+    private final double re;
+    private final double im;
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double realPart() {
+        return re;
+    }
+
+    public double imaginaryPart() {
+        return im;
+    }
+
+    public Complex plus(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    public Complex minus(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+    // Static factory, used in conjunction with private constructor (Page 85)
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public Complex times(Complex c) {
+        return new Complex(re * c.re - im * c.im, re * c.im + im * c.re);
+    }
+
+    public Complex dividedBy(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re * c.im) / tmp);
+    }
+
+    // See page 47 to find out why we use compare instead of ==
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Complex)) {
+            return false;
+        }
+        Complex c = (Complex) o;
+        return Double.compare(re, c.re) == 0 && Double.compare(im, c.im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Double.hashCode(re) + Double.hashCode(im);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + re + " + " + im + ")";
+    }
+}
